2b1b926294e43db392c9887f376384426d36de67
Ljupche <baldovaliev.b@gmail.com>
2024-08-28 13:55:37 +0200

    Initial commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let y;
+
+	const tabs = [
+		{ name: 'Projects', link: '#projects' },
+		{ name: 'About', link: '#about' }
+		// {name: 'Blog', '#blog'}
+	];
+</script>
+
+<header>
+	<h1>
+		<b>Ljupcho</b> Baldovaliev
+	</h1>
+	<div class="">
+		{#each tabs as tab, index}
+			<a>
+				<p>{tab.name}</p>
+			</a>
+		{/each}
+	</div>
+</header>
